5537d0737349e7c4c07c27c5df8fef39462e8a77
Zachary Light <zacharylight15@mittymonarch.com>
2019-02-18 13:50:18 -0800

    updating gitignore

--- main.d
+++
@@ -1,8 +0,0 @@
-main.o: main.cpp SIR.h /usr/local/include/wmtk/WMtk.h \
- /usr/local/include/wmtk/ActorNetwork.h \
- /usr/local/include/wmtk/AggregateFeatureVector.h \
- /usr/local/include/wmtk/FeatureVector.h \
- /usr/local/include/wmtk/ChunkFeatureVector.h \
- /usr/local/include/wmtk/Chunk.h /usr/local/include/wmtk/CriticNetwork.h \
- /usr/local/include/wmtk/StateFeatureVector.h \
- /usr/local/include/wmtk/WorkingMemory.h
